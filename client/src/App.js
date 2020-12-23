a83f1dda345be19590451acebfd9403850733083
Ken Charette <charettekenneth@gmail.com>
2020-12-23 11:58:27 -0500

    Made minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-<<<<<<< HEAD
           <a href="http://localhost:9090/h2-console/">View Database</a>
-=======
-          <a href="http://localhost:8079/h2-console/">View Database</a>
->>>>>>> 8358444e3c7b5e323a0ee6ea0ca427e7b7d44ea2
           <CustomerForm/>
           <Customers/>
         </header>
